f4d2d3239eb0d8f4b8667f5606b517712b77367a
MontMor4 <victorperial@hotmail.com>
2023-07-10 11:53:18 -0300

    testes

--- PlatEMO-master/PlatEMO/Indicadores.m
+++ PlatEMO-master/PlatEMO/Indicadores.m
@@ -22,14 +22,19 @@ IGDp_A1=[];
 for i = 1 : NE        
     IGDp_A1=[IGDp_A1;IGDp2(A1{i}(:,size(A1{i},2)-M+1:size(A1{i},2)),Pareto_Combinado)];    
 end
+Min_IGDp_A1=min(IGDp_A1)
 Media_IGDp_A1=mean(IGDp_A1)
-DP_IGDp_A1=std(IGDp_A1)
+Max_IGDp_A1=max(IGDp_A1)
+%DP_IGDp_A1=std(IGDp_A1)
+
 
 IGDp_A2=[];
 for i = 1 : NE        
     IGDp_A2=[IGDp_A2;IGDp2(A2{i}(:,size(A2{i},2)-M+1:size(A2{i},2)),Pareto_Combinado)];    
 end
+Min_IGDp_A2=min(IGDp_A2)
 Media_IGDp_A2=mean(IGDp_A2)
-DP_IGDp_A2=std(IGDp_A2)
+Max_IGDp_A2=max(IGDp_A2)
+%DP_IGDp_A2=std(IGDp_A2)
 
 ranksum(IGDp_A1,IGDp_A2) % Wilcoxon
--- PlatEMO-master/PlatEMO/definicaoVariaveis.m
+++ PlatEMO-master/PlatEMO/definicaoVariaveis.m
@@ -1,8 +1,16 @@
-problemaMO = @DTLZ1;
-D = 9;
-M = 5;
-N = 212;
-maxFE = 80000;
+problemaMO = @DTLZ4;
+Problema = 'DTLZ4';
 
-NE=2;
-Problema='DTLZ1';
+% M = 5; N = 212;
+% M = 8; N = 156;
+% M = 10; N = 276;
+ M = 15; N = 136;
+
+% DTLZ1 D = (M + K) - 1, com K = 5
+% DTLZ2-4 D = (M + K) - 1, com K = 10
+K = 10; 
+D = M + K - 1;
+MaxGen = 3000;
+maxFE = N * MaxGen;
+
+NE = 20; % numero de execuções
